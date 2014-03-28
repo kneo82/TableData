500d792c6a99c0ae071db63dd0f5a6e5b3b4775f
kneo82 <kneo82@gmail.com>
2014-03-28 08:58:08 +0200

    1

---
+++ TableData/Controller/LoginViewController/TDLoginView.h
@@ -0,0 +1,13 @@
+//
+//  TDLoginView.h
+//  TableData
+//
+//  Created by Vitaliy Voronok on 3/28/14.
+//  Copyright (c) 2014 Vitaliy Voronok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLoginView : UIViewController
+
+@end
---
+++ TableData/Controller/LoginViewController/TDLoginView.m
@@ -0,0 +1,38 @@
+//
+//  TDLoginView.m
+//  TableData
+//
+//  Created by Vitaliy Voronok on 3/28/14.
+//  Copyright (c) 2014 Vitaliy Voronok. All rights reserved.
+//
+
+#import "TDLoginView.h"
+
+@interface TDLoginView ()
+
+@end
+
+@implementation TDLoginView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
